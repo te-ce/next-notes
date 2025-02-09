46381eb08c6820d623a28d71f98221487990b7e3
te-ce <9517685+te-ce@users.noreply.github.com>
2025-02-09 13:42:54 +0100

    feat(ui): cleanup from init

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://www.google.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
+        Google
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://7cworks.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        7cworks.com →
+      </a>
+    </footer>
+  );
+};
